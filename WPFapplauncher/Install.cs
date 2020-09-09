41b2ee89a90f4d1a9d60611b6f647c8a1707b596
Chaitanyassr <chaitanyas0101@gmail.com>
2020-09-09 17:29:11 +0530

    file commited

---
+++ WPFapplauncher/Install.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+
+namespace WPFapplauncher
+   
+{
+    class Installs
+    {
+        public static void Installer(string[] args)
+        {
+
+            //or If the application is zip file then comment the above line and use the below one to extract the files
+            string extractPath = @"c:\example\extract";
+            string zipPath = @"c:\example\result.zip";
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+            Process.Start("regfilepath")
+        }
+    }
+}
+
---
+++ WPFapplauncher/Launch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace WPFapplauncher
+{
+    class Launch
+    {
+        public static void LaunchApp()
+        {
+            // write the URL scheme that you have use in reg file
+            Process.Start("");
+        }
+
+    }
+
+    }
+}
